6f42c482d9d415f62176b9d8707047f24a4c51ba
Chendjou deGrace <chendjou2016@outlook.fr>
2020-01-31 11:09:00 +0100

    Update index.js path

--- lib/expection/argument.out.of.range.exception.ts
+++
@@ -1,7 +0,0 @@
-export class ArgumentOutOfRangeException implements Error {
-    name: string = "ArgumentOutOfRangeException";
-    stack?: string | undefined;
-    constructor(public message = "The given index is out of range of index of the collection"){
-
-    }
-}
--- lib/expection/null.argument.exception.ts
+++
@@ -1,1 +0,0 @@
-export class NullArgumentException extends Error {}
